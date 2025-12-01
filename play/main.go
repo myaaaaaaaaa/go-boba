e3aaec2f35530cb1530b9745651eaea350f42d15
myaaaaaaaaa <103326468+myaaaaaaaaa@users.noreply.github.com>
2025-12-01 04:35:51 -0500

    improve playback experience

--- play/main.go
+++ play/main.go
@@ -7,6 +7,10 @@ import (
 )
 
 func main() {
+	os.Stdout.Write([]byte("\x1b[H\x1b[2J\x1b[3J"))
+	os.Stdout.Write([]byte("\x1b[?25l"))
+	defer os.Stdout.Write([]byte("\x1b[?25h"))
+
 	isClear := regexp.MustCompile("^\x1b" + `\[\d*J`).MatchString
 	for s := range splitBeforeSeq(os.Stdin, "\n\x1b") {
 		if len(s) == 0 {
@@ -18,9 +22,10 @@ func main() {
 			// time.Sleep(time.Millisecond)
 		case '\x1b':
 			if isClear(s) {
-				time.Sleep(time.Millisecond * 300)
+				time.Sleep(time.Millisecond * 100)
 			}
 		}
 		os.Stdout.Write([]byte(s))
 	}
+	time.Sleep(time.Second)
 }
