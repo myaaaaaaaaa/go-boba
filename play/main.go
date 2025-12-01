873b85dc048d7865343b7cec8b041408e05b1b17
myaaaaaaaaa <103326468+myaaaaaaaaa@users.noreply.github.com>
2025-12-01 04:26:58 -0500

    actually implement play

--- play/main.go
+++ play/main.go
@@ -2,12 +2,25 @@ package main
 
 import (
 	"os"
+	"regexp"
 	"time"
 )
 
 func main() {
+	isClear := regexp.MustCompile("^\x1b" + `\[\d*J`).MatchString
 	for s := range splitBeforeSeq(os.Stdin, "\n\x1b") {
-		time.Sleep(time.Millisecond * 200)
+		if len(s) == 0 {
+			continue
+		}
+
+		switch s[0] {
+		case '\n':
+			// time.Sleep(time.Millisecond)
+		case '\x1b':
+			if isClear(s) {
+				time.Sleep(time.Millisecond * 300)
+			}
+		}
 		os.Stdout.Write([]byte(s))
 	}
 }
